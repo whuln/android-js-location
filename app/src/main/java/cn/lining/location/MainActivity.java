1f1ae7120536c8fc1390be17b7d893af4d2ad7ff
whuln <chixin-taxue@163.com>
2018-11-14 17:47:51 +0800

    ContextCom.checkpermission

--- app/src/main/java/cn/lining/location/MainActivity.java
+++ app/src/main/java/cn/lining/location/MainActivity.java
@@ -6,7 +6,9 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
+import android.location.Location;
 import android.location.LocationManager;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -93,9 +95,13 @@ public class MainActivity extends AppCompatActivity {
 
         //
         locationProvider = locationManager.getBestProvider(criteria,true);
-        if(checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
-             ){
-
+        if(ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+            && ContextCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED ){
+            Log.i("lo","onCreate no permissions granted!");
+            return;
         }
+
+
+
     }
 }
